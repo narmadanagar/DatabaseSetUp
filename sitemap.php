1a608a32840e6ee67aeebf7ca9268ac0063f773a
Nidhi Singh <nsingh4@silk2.uvm.edu>
2015-09-18 12:10:08 -0400

    git new project

---
+++ sitemap.php
@@ -0,0 +1,30 @@
+<?php
+/* php code can go anywhere but we will be starting almost all our documents
+ * with php code There is no code here at the moment */
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Nidhi Singh</title>
+        <meta charset="UTF-8">
+        <meta name="description"
+              content="Homepage">
+        <meta name="author"
+              content="Nidhi Singh">
+        <meta name="keywords"
+              content="Index">
+    </head>
+
+
+    <body>
+
+        <h1>Home Page</h1>
+        <p><a href="assignment1.0/index.php">Assignment #1</a></p>
+        <p><a href="tryme.php">Tryme</a></p>
+        
+    </body>
+</html>
+
+
